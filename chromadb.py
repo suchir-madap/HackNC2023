6f3b434437242f148727a1a3f009f90af40a58b3
suchir madap <81061506+suchir-madap@users.noreply.github.com>
2023-10-28 17:25:08 -0400

    adding langchain

---
+++ chromadb.py
@@ -0,0 +1,21 @@
+import chromadb
+chroma_client = chromadb.Client()
+
+collection = chroma_client.create_collection(name="my_collection")
+
+
+client = chromadb.PersistentClient(path="/data")
+
+
+# collection.add(
+#     documents=["This is a document", "This is another document"],
+#     metadatas=[{"source": "my_source"}, {"source": "my_source"}],
+#     ids=["id1", "id2"]
+# )
+
+
+
+# results = collection.query(
+#     query_texts=["This is a query document"],
+#     n_results=2
+# )
