1133c7d9bae511f04b453b28211c0d395e571292
Harrison Enyeart <enyeart@unc.edu>
2023-10-28 19:39:35 -0400

    fixed

--- app2.py
+++ app2.py
@@ -2,12 +2,12 @@ import streamlit as st
 from ingestPDF import readUploadedPdf
 
 
- import io
+import io
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
 
 
- uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 
- readUploadedPdf(uploaded_file)
+readUploadedPdf(uploaded_file)
