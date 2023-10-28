8c49842dd469ceed0f42ebb25688acec0c44f7b9
Harrison Enyeart <enyeart@unc.edu>
2023-10-28 19:28:59 -0400

    fixed app.py

--- app.py
+++ app.py
@@ -63,14 +63,14 @@ def autoplay_audio(file_path: str):
 st.subheader("Welcome to our AI Text Analyzer!")
 st.write("This program is designed to help you answer any questions you have about your uploaded document. Please enter a question/prompt below and the computer will generate a response to answer your query!")
 st.write("Please input your text here, I will analyze it: ")
-txt = st.text_area('Enter your text: ')
+doc = st.text_area('Enter your text: ')
 txt = st.text_area('Enter your question: ')
 
 col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
     if st.button('Play'):
-        textToAudio(txt)
+        textToAudio(doc)
         autoplay_audio("output.mp3")
 with col2:
     st.button('Pause')
